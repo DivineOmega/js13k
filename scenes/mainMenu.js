018cc7db24a7bc60f0b30f5b096adbc7b73d93bc
DivineOmega <jordan@hall05.co.uk>
2015-08-15 23:02:22 +0100

    Interactive main meny

--- scenes/mainMenu.js
+++ scenes/mainMenu.js
@@ -3,10 +3,19 @@ function MainMenu()
 {
     this.convergame = null;
 
+    this.currentMenuIndex = 0;
 
     this.updateFunction = function(time)
     {
+        if (this.convergame.isControlPressed("down") && this.currentMenuIndex<1)
+        {
+            this.currentMenuIndex++;
+        }
         
+        if (this.convergame.isControlPressed("up") && this.currentMenuIndex>0)
+        {
+            this.currentMenuIndex--;
+        }
     };
     
     this.renderFunction = function()
@@ -18,13 +27,26 @@ function MainMenu()
 
         this.convergame.drawText(50, 75, textStyle, 64, textFont, "Reversed Game for JS13K");
 
-        this.convergame.drawText(75, 700, textStyle, 42, textFont, "Start");
-        this.convergame.drawText(75, 800, textStyle, 42, textFont, "Options");
-
         var rectangleStyle = "#FFFFFF";
         
         this.convergame.drawRect(50, 650, 500, 90, rectangleStyle);
         this.convergame.drawRect(50, 750, 500, 90, rectangleStyle);
+        
+        var recentangleFillStyle = "#333333";
+        
+        switch(this.currentMenuIndex)
+        {
+            case 0:
+                this.convergame.drawFilledRect(50, 650, 500, 90, rectangleStyle, recentangleFillStyle);
+                break;
+                
+            case 1:
+                this.convergame.drawFilledRect(50, 750, 500, 90, rectangleStyle, recentangleFillStyle);
+                break;
+        }
+        
+        this.convergame.drawText(75, 700, textStyle, 42, textFont, "Start");
+        this.convergame.drawText(75, 800, textStyle, 42, textFont, "Options");
         
     };
     
