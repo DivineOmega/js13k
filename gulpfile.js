124e3a32902e7dfecb669eaa40824a10e4e13736
Ollie <ojreardon@gmail.com>
2015-08-15 14:49:49 +0100

    Added gulp-jshint and gulp-watch to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
 zip = require('gulp-zip'),
+watch = require('gulp-watch'),
+jshint = require('gulp-jshint'),
 gp_concat = require('gulp-concat'),
 gp_uglify = require('gulp-uglify');
 
-var zip_files = ['js13k.js', 'index.html'],
-	js_files = ['convergame/convergame.js', 'scenes/*'];
+var zip_files = ['js13k.js', 'index.html'], //Files to be added to the zip folder use "<directory goes here>/*" for all files inside the directory
+	js_files = ['convergame/convergame.js', 'scenes/*']; //All your JS files to be combined and minified
 
 gulp.task('zip', function () {
     return gulp.src(zip_files, {base: "."})
@@ -18,4 +20,16 @@ gulp.task('build', function() {
 		.pipe(gulp.dest('./'))
 		.pipe(gp_uglify())
 		.pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(js_files).on('change', function(file) {
+	    return gulp.src(file.path)
+		        .pipe(jshint())
+		        .pipe(jshint.reporter('default'))
+		        .pipe(gp_concat('game.js'))
+				.pipe(gulp.dest('./'))
+				.pipe(gp_uglify())
+				.pipe(gulp.dest('./'));
+	});
 });
