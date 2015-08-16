598c1c006103b9e6fe61bf0f38c85c694494ce88
Ollie <ojreardon@gmail.com>
2015-08-16 23:21:40 +0100

    GULP WATCH! ...And other Gulp tasks such as images, build-html, build-js, etc

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,15 @@ zip = require('gulp-zip'),
 watch = require('gulp-watch'),
 jshint = require('gulp-jshint'),
 gp_concat = require('gulp-concat'),
-gp_uglify = require('gulp-uglify');
+gp_uglify = require('gulp-uglify'),
+imageop = require('gulp-image-optimization'),
+minifyHTML = require('gulp-minify-html'),
+rename = require('gulp-rename'),
+runSequence = require('run-sequence');
 
 var zip_files = ['js13k.js', 'index.html'], //Files to be added to the zip folder use "<directory goes here>/*" for all files inside the directory
-	js_files = ['convergame/convergame.js', 'scenes/*']; //All your JS files to be combined and minified
+	js_files = ['convergame/convergame.js', 'scenes/*'], //All your JS files to be combined and minified
+	img_files = ['assets/*.png','assets/*.jpg','src/**/*.gif','assets/*.jpeg'];
 
 gulp.task('zip', function () {
     return gulp.src(zip_files, {base: "."})
@@ -14,7 +19,16 @@ gulp.task('zip', function () {
         .pipe(gulp.dest('build'));
 });
 
-gulp.task('build', function() {
+//Todo: Test to see if it'll override images
+gulp.task('images', function(cb) {
+    gulp.src(img_files).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('assets')).on('end', cb).on('error', cb);
+});
+
+gulp.task('build-js', function() {
 	return gulp.src(js_files)
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'))
@@ -23,12 +37,31 @@ gulp.task('build', function() {
 		.pipe(gp_uglify())
 		.pipe(gulp.dest('./'));
 });
-/*
-gulp.task('watch', function() {
-	gulp.watch(js_files).on('change', function(file) {
-	    return gulp.src(file.path)
-		        .pipe(jshint())
-		        .pipe(jshint.reporter('default'))
-		        .pipe(gulp.start('build'));
+
+gulp.task('build-html', function() { 
+  return gulp.src('./index.unmin.html')
+    .pipe(minifyHTML())
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('build-dev', function() {
+	return gulp.src(js_files)
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(gp_concat('js13k.js'))
+		.pipe(gulp.dest('./'));
+});
+
+//Legacy 'build' alias for build-dev task
+gulp.task('build', ['build-dev']);
+gulp.task('watch', function () {
+
+	watch(js_files, function () {
+		runSequence('build-dev', 'zip');
 	});
-});*/
+
+	watch('./index.unmin.html', function () {
+		runSequence('build-html', 'zip');
+	});
+});
