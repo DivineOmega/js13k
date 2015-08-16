a8d862397a8e327035619f44111abd7f614932e3
DivineOmega <jordan@hall05.co.uk>
2015-08-16 23:07:57 +0100

    No longer set position based on height of screen

--- scenes/mainMenu.js
+++ scenes/mainMenu.js
@@ -34,24 +34,24 @@ function MainMenu()
 
         this.convergame.drawText(width / 2, 75, textStyle, 64, textFont, "center", "Reversed Game for JS13K", true, 2, 2, "#2c3e50");
 
-        this.convergame.drawRect(50, height - 250, 500, 100, 500, 90, rectangleStyle);
-        this.convergame.drawRect(50, height - 150, 500, 100, 500, 90, rectangleStyle);
+        this.convergame.drawRect(50, 750, 500, 100, 500, 90, rectangleStyle);
+        this.convergame.drawRect(50, 850, 500, 100, 500, 90, rectangleStyle);
         
         var recentangleFillStyle = "#2c3e50";
         
         switch(this.currentMenuIndex)
         {
             case 0:
-                this.convergame.drawFilledRect(50, height - 250, 500, 100, rectangleStyle, recentangleFillStyle); //We would save space by just changing the FillStyle here :) 
+                this.convergame.drawFilledRect(50, 750, 500, 100, rectangleStyle, recentangleFillStyle); //We would save space by just changing the FillStyle here :) 
                 break;
                 
             case 1:
-                this.convergame.drawFilledRect(50, height - 150, 500, 100, rectangleStyle, recentangleFillStyle); //We would save space by just changing the FillStyle here :) 
+                this.convergame.drawFilledRect(50, 850, 500, 100, rectangleStyle, recentangleFillStyle); //We would save space by just changing the FillStyle here :) 
                 break;
         }
         
-        this.convergame.drawText(100, height - 210, textStyle, 42, textFont, "left" ,"Start");
-        this.convergame.drawText(120, height - 110, textStyle, 42, textFont, "left" ,"Options");
+        this.convergame.drawText(100, 810, textStyle, 42, textFont, "left" ,"Start");
+        this.convergame.drawText(120, 910, textStyle, 42, textFont, "left" ,"Options");
     };
     
     this.init = function(convergame)
