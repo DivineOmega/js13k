9a7c47db6f9bbcd081c78ab821cee9b678b5b682
Ollie <ojreardon@gmail.com>
2015-09-12 20:19:52 +0100

    Fixed button positioning

--- scenes/minigameButton.js
+++ scenes/minigameButton.js
@@ -43,12 +43,12 @@ function MinigameButton()
             baseRadius = 256;
         this.convergame.blankCanvas('#1abc9c');
 
-        this.convergame.drawFilledCircle((width / 2) - (baseRadius / 4), height / 2 + 110, baseRadius, '#2c3e50', '#2c3e50');
-        this.convergame.drawFilledCircle((width / 2) - (baseRadius / 4), height / 2 + 100, baseRadius, this.colShadow, this.colShadow);
-        this.convergame.drawFilledCircle((width / 2) - (baseRadius / 4), height / 2 + 100, baseRadius - 2, this.colShadow, this.col);
+        this.convergame.drawFilledCircle(width / 2, height / 2 + 110, baseRadius, '#2c3e50', '#2c3e50');
+        this.convergame.drawFilledCircle(width / 2, height / 2 + 100, baseRadius, this.colShadow, this.colShadow);
+        this.convergame.drawFilledCircle(width / 2, height / 2 + 100, baseRadius - 2, this.colShadow, this.col);
 
         this.convergame.drawText(width / 2, 150, white, 32, font, "center", (this.gameTime-this.timer).toFixed(2), true, 0, 2, "#16a085");
-        this.convergame.drawText((width / 2) - (this.convergame.getTextWidth(this.instructions) / 2), height / 2 + 110, white, 64, font, "center", this.instruction, true, 0, -2, "#2c3e50");
+        this.convergame.drawText(width / 2, height / 2 + 110, white, 64, font, "center", this.instruction, true, 0, -2, "#2c3e50");
     };
     
     this.init = function(convergame)
